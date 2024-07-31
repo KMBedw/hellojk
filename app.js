a288e2528d8641fe2b264813f813d584bac31eab
KMBedw <edwin3@live.fr>
2024-07-31 21:47:09 +0200

    config jenkins

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-// port d'écoute
+// port d'écoutes
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
