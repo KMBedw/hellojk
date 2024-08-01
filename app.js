679ddeaf4b041ee3174974c2b60b33d9bbfa2330
KMBedw <edwin3@live.fr>
2024-08-01 10:25:32 +0200

    test de config pipeline jenkins

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-// port d'écoutes fffff fhfhf
+// port d'écoutes
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
